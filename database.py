19cf33d07f079fe1c1359201f4b48e58b0bb2b80
huynhcuong2233 <huynhcuong251008@gmail.com>
2026-08-06 21:00:42 +0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,138 @@
+import sqlite3
+
+DB_NAME = "data/database.db"
+
+
+def connect():
+    return sqlite3.connect(DB_NAME)
+
+
+def create_database():
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        user_id INTEGER PRIMARY KEY,
+        username TEXT,
+        full_name TEXT,
+        balance INTEGER DEFAULT 0,
+        total_deposit INTEGER DEFAULT 0
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS deposits(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        amount INTEGER,
+        content TEXT,
+        status TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS orders(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        product TEXT,
+        price INTEGER,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_user(user_id, username, full_name):
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT OR IGNORE INTO users(user_id, username, full_name)
+    VALUES (?, ?, ?)
+    """, (user_id, username, full_name))
+
+    conn.commit()
+    conn.close()
+
+
+def get_user(user_id):
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT * FROM users WHERE user_id=?",
+        (user_id,)
+    )
+
+    user = cur.fetchone()
+
+    conn.close()
+    return user
+
+
+def get_balance(user_id):
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT balance FROM users WHERE user_id=?",
+        (user_id,)
+    )
+
+    row = cur.fetchone()
+
+    conn.close()
+
+    if row:
+        return row[0]
+
+    return 0
+
+
+def add_balance(user_id, amount):
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("""
+    UPDATE users
+    SET balance = balance + ?,
+        total_deposit = total_deposit + ?
+    WHERE user_id=?
+    """, (amount, amount, user_id))
+
+    conn.commit()
+    conn.close()
+
+
+def minus_balance(user_id, amount):
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("""
+    UPDATE users
+    SET balance = balance - ?
+    WHERE user_id=?
+    """, (amount, user_id))
+
+    conn.commit()
+    conn.close()
+
+
+def add_deposit(user_id, amount, content, status="pending"):
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT INTO deposits(user_id, amount, content, status)
+    VALUES (?, ?, ?, ?)
+    """, (user_id, amount, content, status))
+
+    conn.commit()
+    conn.close()
+
+
+create_database()
