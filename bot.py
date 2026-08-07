e9fb693413255d1fd1a2fd8c60bc0124bb7f7238
huynhcuong2233 <huynhcuong251008@gmail.com>
2026-08-07 10:16:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
+import asyncio
+
 from app import tg
 
 
+async def main():
+
+    await tg.initialize()
+
+    await tg.start()
+
+    await tg.updater.start_polling()
+
+    print("✅ BOT ONLINE")
+
+    await asyncio.Event().wait()
+
+
 if __name__ == "__main__":
-    tg.run_polling()
+    asyncio.run(main())
